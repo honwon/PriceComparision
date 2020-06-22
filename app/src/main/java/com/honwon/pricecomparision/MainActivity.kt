17b00bf5436205c76ceb1ffe35330561911b5d2e
honwon <moheom98@gmail.com>
2020-06-22 12:16:31 +0900

    제품명 검색까지 구현

--- app/src/main/java/com/honwon/pricecomparision/MainActivity.kt
+++ app/src/main/java/com/honwon/pricecomparision/MainActivity.kt
@@ -2,12 +2,50 @@ package com.honwon.pricecomparision
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.browse
+import org.jetbrains.anko.selector
+import org.jetbrains.anko.toast
+import java.net.URLEncoder
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+
+
+
+
+
+
+
+
+
+        searchBtn.setOnClickListener {
+            val cities = listOf("다나와", "에누리","네이버 쇼핑")
+            val product = URLEncoder.encode(productName.text.toString(),"utf-8")
+
+            selector(title = "온라인 쇼핑몰",items = cities){
+
+                    dialogInterface, i ->
+                when(i) {
+                    0 -> if(product == ""){
+                        toast("제품명이 입력되지 않았습니다")
+                    } else { browse("http://search.danawa.com/mobile/dsearch.php?keyword=${product}")   }
+
+                    1 -> if(product == ""){
+                    toast("제품명이 입력되지 않았습니다")
+                    } else{browse("http://m.enuri.com/m/search.jsp?keyword=${product}")}
+
+                    2 -> if(product == ""){
+                    toast("제품명이 입력되지 않았습니다")
+                } else{browse("https://msearch.shopping.naver.com/search/all?query=${product}&frm=NVSHSRC&cat_id=&pb=true&mall=")
+                }}}
+
+
+        }
 
     }
 }
--- app/src/test/java/com/honwon/pricecomparision/ExampleUnitTest.kt
+++ app/src/test/java/com/honwon/pricecomparision/ExampleUnitTest.kt
@@ -3,6 +3,7 @@ package com.honwon.pricecomparision
 import org.junit.Test
 
 import org.junit.Assert.*
+import java.net.URLEncoder
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -12,6 +13,11 @@ import org.junit.Assert.*
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+        val a = ""
+
+        val b = URLEncoder.encode(a,"euc-kr")
+        val c = URLEncoder.encode(a,"utf-8")
+        println(b)
+        println(c)
     }
 }
